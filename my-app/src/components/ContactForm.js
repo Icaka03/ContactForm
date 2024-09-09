f3204948ffb2326622205b05ccfddf7bab84187e
unknown <generatorfox@gmail.com>
2024-09-09 15:20:09 +0300

    add POST fetch and responsive design

--- my-app/src/components/ContactForm.js
+++ my-app/src/components/ContactForm.js
@@ -1,8 +1,50 @@
 import infoImg from "../images/info-img.png";
 import mailIcon from "../images/mail-icon.png";
+import { useEffect, useState } from "react";
 import "../styles/contactForm.css";
 
 export default function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    telephone: "",
+    topic: "",
+    request: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://server.internal.anjela.info/api/contact_form`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (!response) {
+        console.log("There was an error with the POST");
+      } else {
+        console.log("Form Data Submitted:", formData);
+        alert("Изпратихте успешно вашата заявка");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="main-container">
       <div className="info-box">
@@ -20,23 +62,79 @@ export default function ContactForm() {
       </div>
       <div className="form">
         <h3>Изпрати запитване:</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="flex">
-            <input className="normal-input" placeholder="Име"></input>
-            <input className="normal-input" placeholder="Фамилия"></input>
+            <div className="relative">
+              {" "}
+              <input
+                className="normal-input"
+                placeholder="Име"
+                type="text"
+                value={formData.name}
+                onChange={handleChange}
+                name="name"
+                required
+              ></input>
+              <span className="bullet">*</span>
+            </div>
+            <div>
+              {" "}
+              <input
+                className="normal-input"
+                placeholder="Фамилия"
+                type="text"
+                value={formData.lastName}
+                onChange={handleChange}
+                name="lastName"
+              ></input>
+            </div>
           </div>
           <div className="flex">
-            <input className="normal-input" placeholder="Е-маил"></input>
-            <input className="normal-input" placeholder="Телефон"></input>
+            <div className="relative">
+              {" "}
+              <input
+                className="normal-input"
+                placeholder="Е-маил"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+                name="email"
+                required
+              ></input>
+              <span className="bullet">*</span>
+            </div>
+
+            <input
+              className="normal-input"
+              placeholder="Телефон"
+              type="text"
+              value={formData.telephone}
+              onChange={handleChange}
+              name="telephone"
+            ></input>
           </div>
           <div>
-            <input placeholder="Тема" className="topic-input"></input>
+            <input
+              placeholder="Тема"
+              className="topic-input"
+              type="text"
+              value={formData.topic}
+              onChange={handleChange}
+              name="topic"
+            ></input>
           </div>
           <div>
-            <input placeholder="Запитване" className="request-input"></input>
+            <input
+              placeholder="Запитване"
+              className="request-input"
+              type="text"
+              value={formData.request}
+              onChange={handleChange}
+              name="request"
+            ></input>
           </div>
 
-          <button className="form-button">
+          <button className="form-button" type="submit">
             изпрати <img src={mailIcon} alt="mail-icon" className="mail-icon" />
           </button>
         </form>
